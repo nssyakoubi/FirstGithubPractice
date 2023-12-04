3e1beeaf2085e2802f431b29d0c8847b062a794c
Nelosh <151106526+nssyakoubi@users.noreply.github.com>
2023-12-04 02:42:38 -0500

    add new file

---
+++ PracticGitwithJAVA/src/gitpractice/Gitpracticejava.java
@@ -0,0 +1,8 @@
+package gitpractice;
+
+public class Gitpracticejava {
+	public static void main(String[] args) {
+		System.out.println("my first git in java");
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# FirstGithubPractice
+# FirstGithubPractice222
 i'm practcing my github skills , any update let me know
+Yes some updates 
